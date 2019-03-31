f338a172962298b31a22aedebabc45b9dd90d56d
unknown <juanjosejimenezgarcia>
2019-03-30 23:33:27 -0600

    version 1

---
+++ ZooApp/ZooApp/Animal.swift
@@ -0,0 +1,25 @@
+﻿//
+//  Animal.swift
+//  ZooApp
+//
+//  Created by LABMAC07 on 15/20/19.
+//  Copyright © 2019 Francisco. All rights reserved.
+//
+
+import UIKit
+
+class Animal{
+    var name:String?
+    var des:String?
+    var image:String?
+    
+    
+    init(name:String,des:String,image:String) {
+        self.name=name
+        self.des=des
+        self.image=image
+        
+    }
+    
+
+}
